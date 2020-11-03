af931f379f88d4221e4602649666b4f3f36752f8
RAY <deipalmer99@gmail.com>
2020-11-03 11:01:42 +0000

    Actualizacion

---
+++ FALL GUYS/Assets/Scripts/CambioPantalla.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambioPantalla : MonoBehaviour
+{
+
+    public GameObject pantalla1;
+    public GameObject pantalla2;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Return))
+        {
+            pantalla1.SetActive(false);
+            pantalla2.SetActive(true);
+        }
+    }
+}
