51e8e4de9d959f4e901dfc9bd7e8cad4d23a3049
RAY <deipalmer99@gmail.com>
2020-11-04 11:52:43 +0000

    Menu incorporado

--- FALL GUYS/Assets/Scripts/CambioPantalla.cs
+++ FALL GUYS/Assets/Scripts/CambioPantalla.cs
@@ -7,13 +7,18 @@ public class CambioPantalla : MonoBehaviour
 
     public GameObject pantalla1;
     public GameObject pantalla2;
+    public GameObject objetos;
+    public GameObject barra;
 
     void Update()
     {
+
         if (Input.GetKey(KeyCode.Return))
         {
             pantalla1.SetActive(false);
             pantalla2.SetActive(true);
+            barra.SetActive(true);
+            objetos.SetActive(true);
         }
     }
 }
